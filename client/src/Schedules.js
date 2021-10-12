28b6595aaabf442a8476f6aa9ac7f4b4db41c528
maofan <maofan1983@gmail.com>
2021-10-12 21:39:23 +0800

    Use material-ui date picker to replace the native date picker

--- client/src/Schedules.js
+++ client/src/Schedules.js
@@ -1,12 +1,17 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { gql } from "@apollo/client";
+import "date-fns";
+import DateFnsUtils from "@date-io/date-fns";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import Chip from "@material-ui/core/Chip";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-import TextField from "@material-ui/core/TextField";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from "@material-ui/pickers";
 
 import { timestampToDate } from "./utils";
 
@@ -35,7 +40,10 @@ const useStyles = makeStyles((theme) => ({
     color: theme.palette.text.secondary,
   },
   text: { margin: theme.spacing(2), color: theme.palette.text.secondary },
-  dateInput: { margin: theme.spacing(1) },
+  dateInput: {
+    margin: theme.spacing(1),
+    maxWidth: 140,
+  },
 }));
 
 const SEASON_STATE = ["ACTIVE", "ENDSEASON", "DRAFT", "PRESEASON"];
@@ -43,13 +51,14 @@ const SEASON_STATE = ["ACTIVE", "ENDSEASON", "DRAFT", "PRESEASON"];
 const Schedules = ({ season, setTitle, showMessage, graph_client }) => {
   const classes = useStyles();
   const [schedules, setSchedules] = useState([]);
-  const [fromDate, setFromDate] = useState("");
-  const [toDate, setToDate] = useState("");
+  const [fromDate, setFromDate] = useState(new Date());
+  const [toDate, setToDate] = useState(new Date());
   const [showBanner, setShowBanner] = useState(true);
 
-  const updateSchedules = useCallback(
-    (filterStr) => {
-      const querySchedules = `
+  const searchGames = useCallback(
+    (fromDate, toDate) => {
+      const updateSchedules = (filterStr) => {
+        const querySchedules = `
       query {
         games(orderBy: gameId,
               ${filterStr}){
@@ -74,43 +83,41 @@ const Schedules = ({ season, setTitle, showMessage, graph_client }) => {
         }
       }
       `;
-      return graph_client
-        .query({
-          query: gql(querySchedules),
-        })
-        .then((data) => {
-          setSchedules(data.data.games);
-        });
+        return graph_client
+          .query({
+            query: gql(querySchedules),
+          })
+          .then((data) => {
+            setSchedules(data.data.games);
+          });
+      };
+      fromDate.setHours(0);
+      fromDate.setMinutes(0);
+      fromDate.setSeconds(0);
+      let fromFilter = `, scheduledTime_gte: ${Math.round(
+        fromDate.getTime() / 1000
+      )}`;
+      toDate.setHours(23);
+      toDate.setMinutes(59);
+      toDate.setSeconds(59);
+      let toFilter = `, scheduledTime_lte: ${Math.round(
+        toDate.getTime() / 1000
+      )}`;
+      const filterStr = `where: { season: "${season.seasonId}" ${fromFilter} ${toFilter}}`;
+      updateSchedules(filterStr).catch((e) => showMessage(e.message, true));
     },
-    [graph_client]
+    [season, showMessage, graph_client]
   );
 
   useEffect(() => {
     const init = () => {
       setTitle("Schedules");
       if (graph_client !== null && season.seasonId !== undefined) {
-        const filterStr = `where: { season: "${season.seasonId}", matchRound: ${season.matchRound}}`;
-        updateSchedules(filterStr).catch((e) => showMessage(e.message, true));
+        searchGames(new Date(), new Date());
       }
     };
     init();
-  }, [season, setTitle, showMessage, updateSchedules, graph_client]);
-
-  const searchGames = () => {
-    let fromFilter = "";
-    if (fromDate !== "") {
-      const from = new Date(`${fromDate}T00:00:00`).getTime() / 1000;
-      fromFilter = `, scheduledTime_gte: ${from}`;
-    }
-    let toFilter = "";
-    if (toDate !== "") {
-      const to = new Date(`${toDate}T23:59:59`).getTime() / 1000;
-      toFilter = `, scheduledTime_lte: ${to}`;
-    }
-    const filterStr = `where: { season: "${season.seasonId}" ${fromFilter} ${toFilter}}`;
-    setShowBanner(false);
-    updateSchedules(filterStr).catch((e) => showMessage(e.message, true));
-  };
+  }, [season, searchGames, setTitle, graph_client]);
 
   const displaySchedules = () => {
     return schedules.map((game, index) => {
@@ -189,7 +196,7 @@ const Schedules = ({ season, setTitle, showMessage, graph_client }) => {
         <Grid container justifyContent="center">
           <Grid item>
             <Typography variant="overline" className={classes.text}>
-              Upcoming games
+              games of today
             </Typography>
           </Grid>
         </Grid>
@@ -197,37 +204,51 @@ const Schedules = ({ season, setTitle, showMessage, graph_client }) => {
       <Grid container justifyContent="flex-start">
         {displaySchedules()}
       </Grid>
-      <Grid container justifyContent="center">
-        <Grid item>
-          <TextField
-            value={fromDate}
-            onChange={(e) => setFromDate(e.target.value)}
-            label="Start Date"
-            type="date"
-            className={classes.dateInput}
-            InputLabelProps={{
-              shrink: true,
-            }}
-          />
-        </Grid>
-        <Grid item>
-          <TextField
-            value={toDate}
-            onChange={(e) => setToDate(e.target.value)}
-            label="End Date"
-            type="date"
-            className={classes.dateInput}
-            InputLabelProps={{
-              shrink: true,
-            }}
-          />
-        </Grid>
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
         <Grid container justifyContent="center">
-          <Button color="primary" onClick={searchGames}>
-            search
-          </Button>
+          <Grid item>
+            <KeyboardDatePicker
+              disableToolbar
+              className={classes.dateInput}
+              variant="inline"
+              format="yyyy-MM-dd"
+              margin="normal"
+              label="Start Date"
+              value={fromDate}
+              onChange={(d) => setFromDate(d)}
+              KeyboardButtonProps={{
+                "aria-label": "change date",
+              }}
+            />
+          </Grid>
+          <Grid item>
+            <KeyboardDatePicker
+              disableToolbar
+              className={classes.dateInput}
+              variant="inline"
+              format="yyyy-MM-dd"
+              margin="normal"
+              label="End Date"
+              value={toDate}
+              onChange={(d) => setToDate(d)}
+              KeyboardButtonProps={{
+                "aria-label": "change date",
+              }}
+            />
+          </Grid>
+          <Grid container justifyContent="center">
+            <Button
+              color="primary"
+              onClick={() => {
+                searchGames(fromDate, toDate);
+                setShowBanner(false);
+              }}
+            >
+              search
+            </Button>
+          </Grid>
         </Grid>
-      </Grid>
+      </MuiPickersUtilsProvider>
     </div>
   );
 };
